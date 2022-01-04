0f30d78d6b3eab53abdd9b21b1b06a4edd002726
dZayr <91765472+dZayre@users.noreply.github.com>
2022-01-04 14:16:50 +0800

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ if (window.innerWidth > 400) {
 
 if (window.innerWidth > 400) {
     function works() {
-        scrollTo({top: 1218, behavior:'smooth'})
+        scrollTo({top: 1293, behavior:'smooth'})
     }
 }else {
     function works() {
